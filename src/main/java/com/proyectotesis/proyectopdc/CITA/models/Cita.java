cb1df96b3badfc5280a473c61191561fdef97c10
Cosimo Ianni <cosimo.ianni@inacapmail.cl>
2023-04-30 06:51:12 -0400

    Se modifica fecha y se agrega hora a Cita, estos son almacenados de forma exitosa en la BBDD. Se comenzaran las pruebas con Mockito para volver a verificar la funcionalidad de cada servicio.

--- src/main/java/com/proyectotesis/proyectopdc/CITA/models/Cita.java
+++ src/main/java/com/proyectotesis/proyectopdc/CITA/models/Cita.java
@@ -4,6 +4,7 @@ package com.proyectotesis.proyectopdc.CITA.models;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
 
@@ -19,8 +20,15 @@ public class Cita {
     @Getter@Setter @Column(name = "id_paciente")
     private int idPaciente;
 
+    @Temporal(TemporalType.DATE)
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     @Getter@Setter @Column(name = "fecha")
     private Date fecha;
+
+    @Temporal(TemporalType.TIME)
+    @DateTimeFormat(pattern = "HH:mm")
+    @Getter@Setter @Column(name = "hora")
+    private String hora;
 
     @Getter@Setter @Column(name = "tipo")
     private String tipo;
